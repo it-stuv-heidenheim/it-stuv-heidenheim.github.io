5c8c28c72bbeae1af9bfddbe4627b065dc9dda96
Bernd Mayinger <berndmayinger@gmail.com>
2022-05-31 21:55:27 +0200

    some basic stuff to sample integr

--- sampleintegration/index.js
+++ sampleintegration/index.js
@@ -1,1 +1,9 @@
-alert("Cool");
+const cardCode = "bUqqHU2g";
+
+const url = `https://api.trello.com/1/cards/${cardCode}/`;
+
+fetch(url).then((res) => {
+  res.json().then((data) => {
+    console.warn(data);
+  });
+});

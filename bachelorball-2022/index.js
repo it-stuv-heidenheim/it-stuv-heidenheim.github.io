33a6c12172c2f3b20d60a6fc255854719a77d43f
Bernd Mayinger <berndmayinger@gmail.com>
2022-06-04 09:20:19 +0200

    added bachelorball 2022 files

---
+++ bachelorball-2022/index.js
@@ -0,0 +1,23 @@
+/* CONFIG SECTION */
+
+const cardCode = "T3dsxBnm";
+const htmlAnchorId = "trello-integration-anchor";
+
+/* LOGIC SECTION */
+
+const url = `https://api.trello.com/1/cards/${cardCode}`;
+
+const fallbackText =
+  "There hasn't been added any content yet. Please come back again later!";
+
+fetch(url).then((res) => {
+  res.json().then((cardData) => {
+    var htmlAnchor = document.querySelector(`#${htmlAnchorId}`);
+    htmlAnchor.innerHTML = processCardDataAndGetText(cardData);
+  });
+});
+
+function processCardDataAndGetText(cardData) {
+  var resHtml = `<p>${cardData.desc || fallbackText}</p>`;
+  return resHtml;
+}

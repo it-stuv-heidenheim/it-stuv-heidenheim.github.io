74954ef7177cd2a931a0b3d79f9d0849a22f1a6f
Div05 <divyansh1999.ds@gmail.com>
2022-06-06 15:53:01 +0200

    Linktree

---
+++ Linktree/index.js
@@ -0,0 +1,23 @@
+/* CONFIG SECTION */
+
+const cardCode = "zawJegnv";
+const htmlAnchorId = "trello-linktree";
+
+/* LOGIC SECTION */
+
+const url = `https://api.trello.com/1/cards/${cardCode}`;
+
+const fallbackText =
+  "There hasn't been added any content yet. Please come back again later!";
+
+fetch(url).then((res) => {
+  res.json().then((cardData) => {
+    var htmlAnchor = document.querySelector(`#${htmlAnchorId}`);
+    htmlAnchor.innerHTML = processCardDataAndGetText(cardData);
+  });
+});
+
+function processCardDataAndGetText(cardData) {
+  var resHtml = `<p>${cardData.desc || fallbackText}</p>`;
+  return resHtml;
+}

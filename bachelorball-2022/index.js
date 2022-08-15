4ea1f6371bc98f9e6492848cdddcfedccf60bc8b
Bernd Mayinger <berndmayinger@gmail.com>
2022-08-15 23:29:20 +0200

    re-thinked logik of bachball: collecting data from single card, with special heading syntax as separators

--- bachelorball-2022/index.js
+++ bachelorball-2022/index.js
@@ -1,31 +1,6 @@
 /* CONFIG SECTION */
 
-const mapping = [
-  {
-    trelloCardCode: "T3dsxBnm",
-    htmlIdSuffix: "faq",
-  },
-  {
-    trelloCardCode: "5x1SqNDI",
-    htmlIdSuffix: "allgemeines",
-  },
-  {
-    trelloCardCode: "j5fFddpq",
-    htmlIdSuffix: "location",
-  },
-  {
-    trelloCardCode: "KpMuPESO",
-    htmlIdSuffix: "motto",
-  },
-  {
-    trelloCardCode: "xvuIEuvn",
-    htmlIdSuffix: "tickets",
-  },
-  {
-    trelloCardCode: "22Oz8NkQ",
-    htmlIdSuffix: "fotos",
-  },
-];
+const trelloCardCode = "ER1gWlYv";
 
 // Text to be shown if label is hidden
 const hiddenText = `Für diesen Bereich stehen noch keine Daten zur Verfügung.
@@ -34,31 +9,90 @@ Bitte versuche es später erneut.`;
 // Text to be shown if label is hidden but no description set
 const noContentYetText = hiddenText;
 
-mapping.forEach((mapObj) => {
-  const { trelloCardCode, htmlIdSuffix } = mapObj;
+const url = `https://api.trello.com/1/cards/${trelloCardCode}`;
 
-  const url = `https://api.trello.com/1/cards/${trelloCardCode}`;
+const fallbackText =
+  "There hasn't been added any content yet. Please come back again later!";
 
-  const fallbackText =
-    "There hasn't been added any content yet. Please come back again later!";
+const sectionNodePrefix = "bachball_2022_";
 
-  fetch(url).then((res) => {
-    res.json().then((cardData) => {
-      var htmlAnchor = document.querySelector(`#bachball-2022-${htmlIdSuffix}`);
+fetch(url).then((res) => {
+  res.json().then((cardData) => {
+    if (!cardData.desc) {
+      alert(`Kritischer Fehler: 
+      überhaupt keine Daten gefunden, bitte sei so gut und melde das dem Orga-Team`);
+    }
+    var lines = cardData.desc.split("\n");
 
-      var innerHtml = "";
-      if (
-        cardData.labels.find((labelObj) => labelObj.color == "red") != undefined
-      ) {
-        // red label set, so content is not supposed to be published yet
-        innerHtml = `<p>${hiddenText}</p>`;
+    var sectionHeadingPattern = RegExp(`^\/\/\/ *(?<sectionName>\\w+) *`);
+
+    var hiddenSectionPattern = RegExp(`^\/\/\/ *(?<sectionName>\\w+) *#+$`);
+
+    var contentOfCurrentSection = "";
+
+    var currentHeading = "";
+    var isCurrentSectionSupposedToBeHidden = false;
+
+    var firstHeadingHandled = false;
+    for (var i = 0; i < lines.length; i++) {
+      var line = lines[i].trim();
+
+      if (line == "") continue;
+
+      if (line.match(sectionHeadingPattern)) {
+        var heading = line
+          .match(sectionHeadingPattern)
+          .groups.sectionName.toLowerCase();
+
+        if (currentHeading == "" && !firstHeadingHandled) {
+          // in case we're currently detecting the first heading
+          firstHeadingHandled = true;
+          currentHeading = heading;
+        }
+
+        if (heading != currentHeading) {
+          // new heading, so content of old section is complete, we can handle it
+
+          if (isCurrentSectionSupposedToBeHidden) {
+            var htmlContent = hiddenText;
+          } else {
+            var htmlContent = markdownParser(contentOfCurrentSection);
+          }
+
+          try {
+            document.querySelector(
+              `#${sectionNodePrefix + currentHeading}`
+            ).innerHTML = htmlContent;
+
+            // and empty the content again
+            contentOfCurrentSection = "";
+          } catch {
+            debugger;
+          }
+
+          currentHeading = heading;
+          isCurrentSectionSupposedToBeHidden =
+            line.match(hiddenSectionPattern) != null;
+        }
       } else {
-        // act normal and process card description
-        innerHtml = processSwitchAndReturnHtml(htmlIdSuffix, cardData.desc);
-      }
+        // no heading, so section content is assumed
+        // we add the content to handle it later
 
-      htmlAnchor.innerHTML = innerHtml;
-    });
+        contentOfCurrentSection += line;
+      }
+    }
+
+    // when lines done, add last section
+
+    var htmlContent = markdownParser(contentOfCurrentSection);
+
+    try {
+      document.querySelector(
+        `#${sectionNodePrefix + currentHeading}`
+      ).innerHTML = htmlContent;
+    } catch {
+      debugger;
+    }
   });
 });
 
@@ -96,28 +130,12 @@ function processCardDataAndGetText(cardDesc) {
   return `<h6>${sanitizeHtml(cardDesc) || noContentYetText}</h6>`;
 }
 
-const faqTemplate = (question, answer) => {
-  return `<div><h6>${question}</h6>
-  <p>${answer}</p>
-  </div>`;
+const markdownParser = (text) => {
+  const toHTML = text
+    .replace(/^### (.*$)/gim, "<h3>$1</h3>") // h3 tag
+    .replace(/^## (.*$)/gim, "<h2>$1</h2>") // h2 tag
+    .replace(/^# (.*$)/gim, "<h1>$1</h1>") // h1 tag
+    .replace(/\*\*(.*)\*\*/gim, "<b>$1</b>") // bold text
+    .replace(/\*(.*)\*/gim, "<i>$1</i>"); // italic text
+  return toHTML.trim(); // using trim method to remove whitespace
 };
-
-function generateFaqHtml(cardDesc) {
-  var lines = cardDesc.split("\n");
-  var i = 0;
-  var resHtml = "";
-  for (i; i < lines.length; i++) {
-    var line = lines[i];
-    if (line == "") continue;
-    var lineParts = line.split("-");
-
-    // if no - in there, no answer set yet, skip the line
-    if (lineParts.length < 2) continue;
-
-    var question = lineParts[0].trim();
-    var answer = lineParts[1].trim();
-    resHtml += faqTemplate(question, answer);
-  }
-
-  return resHtml;
-}

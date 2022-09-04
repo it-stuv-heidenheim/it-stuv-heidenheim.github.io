0999e46add2b3c6b67a33475f8c50ae4697712e4
Bernd Mayinger <berndmayinger@gmail.com>
2022-09-04 20:45:53 +0200

    you can now config whether a card should be hidden if in past

--- calendar/index.js
+++ calendar/index.js
@@ -1,52 +1,67 @@
 /* CONFIG SECTION */
 
-const apiToken = "0TNxIxk3ETO9T2BfMZ9S821C";
-const trelloBoardId = "61e0727e01aa1e19ecb7adbf";
+const apiToken = "0TNxIxk3ETO9T2BfMZ9S821C"
+const trelloBoardId = "61e0727e01aa1e19ecb7adbf"
 
-const htmlAnchorId = "trello-calendar";
-
-/* LOGIC SECTION */
-
-const url = `https://api.trello.com/1/boards/${trelloBoardId}/cards?token=${apiToken}`;
-
+const htmlAnchorId = "trello-calendar"
 const fallbackText =
-  "There hasn't been added any content yet. Please come back again later!";
+  "There hasn't been added any content yet. Please come back again later!"
 
-const noDescriptionText = "- keine Beschreibung veröffentlicht -";
+const noDescriptionText = ""
+const skipUntilThisMonth = false
 
-var htmlOut = "";
+/* END OF CONFIG SECTION */
+
+const url = `https://api.trello.com/1/boards/${trelloBoardId}/cards?token=${apiToken}`
+
+var htmlOut = ""
+
+var now = new Date()
+var thisMonth = now.getMonth(),
+  thisYear = now.getFullYear()
 
 fetch(url).then((res) => {
   res.json().then((cards) => {
     // filter the ones without start date
-    cards = cards.filter((card) => card.start != null);
+    cards = cards.filter((card) => card.start != null)
 
     // sort the cards by their start date (comparison with plain hyphen unfortunately didn't work)
-    cards.sort((a, b) => new Date(a.start) - new Date(b.start));
+    cards.sort((a, b) => new Date(a.start) - new Date(b.start))
 
-    var iterMonthCode = "";
+    var iterMonthCode = ""
 
     for (var i = 0; i < cards.length; i++) {
-      var card = cards[i];
+      var card = cards[i]
 
-      const { name, desc, start, labels } = card;
+      const { name, desc, start, labels } = card
 
-      if (labels.length > 0) continue;
+      if (labels.length > 0) continue
 
       // just very basic reformatting
 
-      var date = new Date(start);
+      var date = new Date(start)
+
+      var monthOfCard = date.getMonth(),
+        yearOfCard = date.getFullYear()
+
+      // skip this card if event in past and accordingly configured before
+
+      if (skipUntilThisMonth) {
+        if (!(yearOfCard >= thisYear && monthOfCard >= thisMonth)) {
+          continue
+        }
+      }
 
       // TODO pretty display
 
-      if (iterMonthCode != date.getMonth() + "-" + date.getFullYear()) {
-        iterMonthCode = date.getMonth() + "-" + date.getFullYear();
+      if (iterMonthCode != `${monthOfCard}-${yearOfCard}`) {
+        iterMonthCode = `${monthOfCard}-${yearOfCard}`
 
         var monthHeading = date.toLocaleDateString("de-de", {
           month: "long",
-        });
+        })
 
-        htmlOut += `<h3 style="margin-bottom: 1em">${monthHeading}</h3>`;
+        htmlOut += `<h3 style="margin-bottom: 1em">${monthHeading}</h3>`
       }
 
       var dateFormatted = date
@@ -55,15 +70,15 @@ fetch(url).then((res) => {
           weekday: "short",
         })
         .toUpperCase()
-        .replace(/(\w+)\., (\d+)\./g, "$1 $2");
+        .replace(/(\w+)\., (\d+)\./g, "$1 $2")
 
       htmlOut += `
       <h4 style="color: #E2001A">${dateFormatted}</h4>
       <h3>${name}</h3>
       <p style="margin-bottom: 3em">${desc || noDescriptionText}</p>
-      `;
+      `
     }
 
-    document.querySelector("#" + htmlAnchorId).innerHTML = htmlOut;
-  });
-});
+    document.querySelector("#" + htmlAnchorId).innerHTML = htmlOut
+  })
+})
